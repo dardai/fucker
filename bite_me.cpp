250fe1616dc5d91e15b5e0920ce5a1eb0e8a7086
daijieboe <daijie@boe.com.cn>
2016-08-15 09:03:55 +0800

    1

---
+++ bite_me.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "Sales_item.h"
+
+int main() {
+	// comment on Aug.11
+	//Sales_item currentT;
+	//	if (std::cin >> currentT) {
+	//		Sales_item nextT;
+	//		while (std::cin >> nextT) {
+	//			if (currentT.isbn() == nextT.isbn()) {
+	//				currentT += nextT;
+	//			}
+	//			else {
+	//				std::cout << "The transaction record summary is " << currentT << std::endl;
+	//				currentT = nextT;
+	//			}
+	//		}
+	//		std::cout << "The transaction record summary is " << currentT << std::endl;
+	//	}
+	//	else {
+	//		std::cerr << "No data my dear~" << std::endl;
+	//		return -1;
+	//	}
+	//	;
+	//	/*	std::cout << b1 <<std::endl<< b2<<std::endl;
+	//		std::cout << b1 + b2 << std::endl;*/
+	//	return 0;
+	std::string domo("Brace is also ok");
+	std::string demo = "dsa";
+	std::cout << demo << domo << "2\t\014\n";
+	return 0;
+}
