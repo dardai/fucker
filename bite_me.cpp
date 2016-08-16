077dc1c683c38410e9c22c7cf6d1d1746b60ae8e
daijie <daijienk@qq.com>
2016-08-16 11:09:38 +0800

    8.16

--- bite_me.cpp
+++ bite_me.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "Sales_item.h"
-
 int main() {
 	// comment on Aug.11
 	//Sales_item currentT;
@@ -25,8 +24,14 @@ int main() {
 	//	/*	std::cout << b1 <<std::endl<< b2<<std::endl;
 	//		std::cout << b1 + b2 << std::endl;*/
 	//	return 0;
-	std::string domo("Brace is also ok");
+	std::string _A_omo("Brace is also ok");
 	std::string demo = "dsa";
-	std::cout << demo << domo << "2\t\014\n";
+	int a = 100, sum = 0, &b = sum, *d = &a, **e = &d;
+	//for (int a = 0; a < 10; a++)
+	//	b += a;
+	int *c = &a;
+	*c = *d***e;
+	std::cout << a << "\n" << *c << "\n";
+	std::cout << demo << _A_omo << "2\t\014\n";
 	return 0;
 }
